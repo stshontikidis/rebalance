0ad0a5a952cda656a6083ca30263f15ff5b7915a
Sava Tshontikidis <savatshontikidis@Savas-MacBook-Pro.local>
2019-08-01 22:48:35 -0400

    Missed adding reblance last commit which has all the info

--- rebalance.py
+++ rebalance.py
@@ -1,141 +1,213 @@
-import sqlite3
+import csv
+import os
 
 import yahoofinancials
 import prettytable
 
-
-conn = sqlite3.connect('finances.db')
-db_cursor = conn.cursor()
+import database as db
+import models.allocation
 
 
 def main():
     action = None
-    while action != 6:
+    while action != 4:
         action = options()
 
         if action == 1:
-            ticker = get_ticker()
-            add_fund(ticker)
-            update_shares(ticker)
-            set_target_allocation(ticker)
-        if action == 2:
-            ticker = get_ticker()
-            update_shares(ticker)
-        if action == 3:
-            ticker = get_ticker()
-            set_target_allocation(ticker)
-        if action == 4:
-            rebalance()
-        if action == 5:
             show_allocation()
-
-        conn.commit()
+        elif action == 2:
+            rebalance()
+        elif action == 3:
+            sheet = get_csv()
+            import_assets(sheet)
 
 
 def rebalance():
     contribution_amount = input('How much are you contributing? ')
 
-    funds = db_cursor.execute('SELECT ticker, shares, target_allocation FROM funds').fetchall()
-
-    tickers = [fund[0] for fund in funds]
+    funds = db.session.query(models.allocation.Asset).all()
+    tickers = [fund.id for fund in funds]
 
     fin = yahoofinancials.YahooFinancials(tickers)
     current_price_map = fin.get_current_price()
-    current_price_map['CASHX'] = 1
+    current_price_map['SPAXX**'] = 1
 
     fund_dict = {}
     protfolio_value = 0.0
 
     for fund in funds:
-        fund_dict[fund[0]] = {
-            'shares': fund[1],
-            'current_price': current_price_map[fund[0]],
-            'current_value': fund[1] * current_price_map[fund[0]],
-            'target_allocation': fund[2]
+        fund_dict[fund.id] = {
+            'shares': fund.shares,
+            'current_price': current_price_map[fund.id],
+            'current_value': fund.shares * current_price_map[fund.id],
+            'target_allocation': 0
         }
 
-        protfolio_value += fund_dict[fund[0]]['current_value']
+        protfolio_value += fund_dict[fund.id]['current_value']
 
     table = prettytable.PrettyTable()
-    table.field_names = ['TICKER', 'SHARES', 'CURRENT_ALLOCATION', 'TARGET_ALLOCATION', 'BUY_AMOUNT']
+    table.field_names = ['TICKER', 'CURRENT_ALLOCATION', 'TARGET_ALLOCATION', 'BUY_AMOUNT']
 
-    for ticker, fund_data in fund_dict.items():
-        target_value = (protfolio_value + float(contribution_amount)) * (fund_data['target_allocation'] / 100)
-        buy_amount = round(target_value - fund_data['current_value'], 2)
+    allocations = db.session.query(models.allocation.Allocation).all()
+    for alloc in allocations:
+        target_value = (protfolio_value + float(contribution_amount)) * (alloc.target / 100)
 
-        current_allocation = round((fund_data['current_value'] / protfolio_value) * 100, 2)
+        active_asset = None
+        current_value = 0.0
+        for asset in alloc.assets:
+            current_value += fund_dict[asset.id]['current_value']
+            if asset.is_active:
+                active_asset = asset
 
-        table.add_row([ticker, fund_data['shares'], current_allocation, fund_data['target_allocation'], buy_amount])
+        current_allocation = round((current_value / protfolio_value) * 100, 2)
+        buy_amount = round(target_value - current_value, 2)
+
+        table.add_row([active_asset.id, current_allocation, alloc.target, buy_amount])
 
     print(table)
     print('\n')
 
 
-def get_ticker():
-    return input('Fund ticker: ')
+def add_or_update_asset(ticker, shares, name):
+    asset = db.session.query(models.allocation.Asset).get(ticker)
+
+    if asset:
+        if shares and asset.shares != shares:
+            asset.shares = shares
+        if name and asset.name != name:
+            asset.name = name
+        print(f'Fund {ticker} was updated')
+    else:
+        asset = models.allocation.Asset(id=ticker, shares=shares, name=name)
+        db.session.add(asset)
+        print(f'Fund with the ticker {ticker} added')
+
+    db.session.flush()
+
+    set_allocation(asset)
+
+    trues = ['y', 'Y', 'yes', 'Yes', 'YES']
+    _input = input('Is this an actively purchased asset y/n: ')
+    asset.is_active = _input in trues
+
+    db.session.flush()
 
 
-def add_fund(ticker):
-    db_cursor.execute('INSERT INTO funds (ticker, shares) VALUES (?, 0);', (ticker, ))
-    print('Fund with the ticker {} added'.format(ticker))
+def set_allocation(asset):
+    current_allocations = db.session.query(models.allocation.Allocation). \
+        order_by(models.allocation.Allocation.name).all()
+
+    if not asset.allocation:
+        table = prettytable.PrettyTable()
+        table.field_names = ['ID', 'NAME', 'TARGET_ALLOCATION']
+
+        for alloc in current_allocations:
+            table.add_row([alloc.id, alloc.name, alloc.target])
+
+        print(f'No current allocation for {asset.id} {asset.name}\n\n\n')
+        print('Current available allocation groups')
+        print(table)
+        print('\n')
+
+        _input = input('Enter ID of alloaction group to add current asset to or desired Name,target for new.\n')
+
+        try:
+            allocation_id = int(_input)
+        except ValueError:
+            name, target = _input.split(',')
+
+            allocation_obj = models.allocation.Allocation(name=name, target=target)
+            db.session.add(allocation_obj)
+            db.session.flush()
+            allocation_id = allocation_obj.id
+
+        allocation_relationship = models.allocation.AssetAllocationRelationship(
+            asset_id=asset.id, allocation_id=allocation_id)
+
+        db.session.add(allocation_relationship)
+        db.session.flush()
+    else:
+        print(f'Current target allocation group {asset.allocation.name} with target {asset.allocation.target}')
+
+    db.session.commit()
 
 
-def update_shares(ticker):
-    shares = input('Number of shares for fund {}: '.format(ticker))
-    if shares:
-        db_cursor.execute('UPDATE funds SET shares=? WHERE ticker=?', (shares, ticker))
+def import_assets(sheet):
+    column_map = {}
+    with open(sheet, newline='') as csv_file:
+        reader = csv.reader(csv_file)
+        column_names = next(reader)
+
+        for idx, name in enumerate(column_names):
+            if name == 'Symbol':
+                column_map['ticker'] = idx
+            elif name == 'Description':
+                column_map['name'] = idx
+            elif name == 'Quantity':
+                column_map['shares'] = idx
+        for asset in reader:
+            if column_map['ticker'] >= len(asset):
+                break
+
+            add_or_update_asset(asset[column_map['ticker']], asset[column_map['shares']], asset[column_map['name']])
 
 
-def set_target_allocation(ticker):
-    target_allocation = input('Desired allocation of fund {}: '.format(ticker))
-    if target_allocation:
-        db_cursor.execute('UPDATE funds SET target_allocation=? WHERE ticker=?', (float(target_allocation), ticker))
+def get_csv():
+    path = './upload'
+    file_name = os.listdir('./upload')[0]
+
+    return path + '/' + file_name
 
 
 def show_allocation():
-    funds = db_cursor.execute('SELECT ticker, shares, target_allocation FROM funds').fetchall()
+    # TODO replace with orm
+    # funds = db_cursor.execute('SELECT ticker, shares, target_allocation FROM funds').fetchall()
 
-    tickers = [fund[0] for fund in funds]
+    funds = db.session.query(models.allocation.Asset).all()
+    tickers = [fund.id for fund in funds]
 
     fin = yahoofinancials.YahooFinancials(tickers)
     current_price_map = fin.get_current_price()
-    current_price_map['CASHX'] = 1
+    current_price_map['SPAXX**'] = 1
 
     fund_dict = {}
     protfolio_value = 0.0
     for fund in funds:
-        fund_dict[fund[0]] = {
-            'shares': fund[1],
-            'current_price': current_price_map[fund[0]],
-            'current_value': fund[1] * current_price_map[fund[0]]
+        fund_dict[fund.id] = {
+            'shares': fund.shares,
+            'current_price': current_price_map[fund.id],
+            'current_value': fund.shares * current_price_map[fund.id]
         }
 
-        protfolio_value += fund_dict[fund[0]]['current_value']
+        protfolio_value += fund_dict[fund.id]['current_value']
+
+    allocations = db.session.query(models.allocation.Allocation).all()
 
     table = prettytable.PrettyTable()
-    table.field_names = ['TICKER', 'SHARES', 'CURRENT_ALLOCATION', 'TARGET_ALLOCATION']
-    for fund in funds:
-        ticker = fund[0]
-        shares = fund[1]
+    table.field_names = ['NAME', 'CURRENT_ALLOCATION', 'TARGET_ALLOCATION', 'ACTIVE FUND']
+    for alloc in allocations:
+        current_allocation = 0.0
+        active_asset = None
+        for asset in alloc.assets:
+            current_allocation += round((fund_dict[asset.id]['current_value'] / protfolio_value) * 100, 2)
+            if asset.is_active:
+                active_asset = asset
 
-        current_allocation = round((fund_dict[ticker]['current_value'] / protfolio_value) * 100, 2)
-        table.add_row([ticker, shares, current_allocation, fund[2]])
+        table.add_row([alloc.name, current_allocation, alloc.target, active_asset.name])
 
     print(table)
     print('\n')
 
 
 def options():
-    valid_options = list(range(1, 7))
+    valid_options = list(range(1, 5))
 
     def print_options():
         print('What would you like to do?')
-        print('1 Add Fund')
-        print('2 Update fund shares')
-        print('3 Set target allocation for fund')
-        print('4 Calcuate purches amounts for current allocation model')
-        print('5 Show current funds')
-        print('6 Exit\n')
+        print('1 Show current funds')
+        print('2 Calcuate purches amounts for current allocation model')
+        print('3 Upload')
+        print('4 Exit\n')
 
     no_selection = True
     while no_selection:
